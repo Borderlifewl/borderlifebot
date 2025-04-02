5169cb750e9f5fc71c828f8a944e1ed7f1f172ea
Borderlifewl <borderlifewl@gmail.com>
2025-04-02 16:19:11 +0200

    Update member_join.py

--- cogs/member_join.py
+++ cogs/member_join.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from config import GUILD_ID, JOIN_CHANNEL_ID
+import pytz
 
 class MemberJoin(commands.Cog):
     def __init__(self, bot):
@@ -26,7 +27,9 @@ class MemberJoin(commands.Cog):
             embed.add_field(name="Nom d'utilisateur", value=member.name, inline=False)
             embed.add_field(name="Pseudo sur le serveur", value=member.display_name, inline=False)
             embed.add_field(name="Création du compte", value=member.created_at.strftime('%d/%m/%Y à %H:%M:%S'), inline=False)
-            embed.add_field(name="Date d'arrivée sur le serveur", value=member.joined_at.strftime('%d/%m/%Y à %H:%M:%S'), inline=False)
+            paris_tz = pytz.timezone('Europe/Paris')
+            joined_at_paris = member.joined_at.astimezone(paris_tz)
+            embed.add_field(name="Date d'arrivée sur le serveur", value=joined_at_paris.strftime('%d/%m/%Y à %H:%M:%S'), inline=False)
             embed.add_field(name="Discord du joueur", value=member.mention, inline=False)
             embed.add_field(name="Admin", value="✅ Oui" if member.guild_permissions.administrator else "❌ Non", inline=False)
             embed.add_field(name="Lien du profil", value=f"[Clique ici](https://discord.com/users/{member.id})", inline=False)
